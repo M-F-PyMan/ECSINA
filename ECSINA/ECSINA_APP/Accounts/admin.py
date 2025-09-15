3cd8878e34a3ac35ca6078cc77e4e77131f0eae9
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-09-16 00:48:48 +0330

    Update admin.py

--- ECSINA/ECSINA_APP/Accounts/admin.py
+++ ECSINA/ECSINA_APP/Accounts/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import User,UserManager,TempUser,Profile
+from .models import User,TempUser,Profile
 
-admin.site.register(User),
-admin.site.register(UserManager),
-admin.site.register(TempUser),
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'name', 'mobile', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'name', 'mobile', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'mobile', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'name', 'mobile')
+    ordering = ('email',)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(TempUser)
 admin.site.register(Profile)
 
