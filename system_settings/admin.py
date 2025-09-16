a00222a50a6069408c779e445aa165f23e083319
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-09-16 21:06:11 +0330

    Delete system_settings directory

--- system_settings/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import SystemSetting
-
-admin.site.register(SystemSetting)
--- system_settings/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import SystemSetting
-
-class SystemSettingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SystemSetting
-        fields = ['id', 'key', 'value']
