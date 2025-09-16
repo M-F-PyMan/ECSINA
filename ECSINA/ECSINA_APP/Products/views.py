04277b88852636ea0833743c797792261135ba27
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-09-17 00:22:52 +0330

    Update views.py

--- ECSINA/ECSINA_APP/Products/views.py
+++ ECSINA/ECSINA_APP/Products/views.py
@@ -1,17 +1,19 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
-from .models import Product, Category,Image
-from .serializers import ProductSerializer, CategorySerializer,ImageSerializer
-
-class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+from .models import Product, Category, Image
+from .serializers import ProductSerializer, CategorySerializer, ImageSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
 
-
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+    permission_classes = [IsAuthenticated]
