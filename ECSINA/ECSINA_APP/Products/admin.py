d8ef871524b1ee7e43b579c3a888ce72b8bb563b
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-09-16 20:45:42 +0330

    Update admin.py

--- ECSINA/ECSINA_APP/Products/admin.py
+++ ECSINA/ECSINA_APP/Products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category,Image
 
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Image)
