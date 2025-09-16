6bd4db9e144b14d2c62c4cf2450ad7e5d950d0b5
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-09-17 00:09:15 +0330

    Update urls.py

--- ECSINA/ECSINA_APP/ECSINA_APP/urls.py
+++ ECSINA/ECSINA_APP/ECSINA_APP/urls.py
@@ -9,12 +9,14 @@ from Accounts import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/accounts/', include('accounts.urls')),
+    path('api/v1/products/', include('products.urls')),
+    path('api/v1/system/', include('system_settings.urls')),
+    path('api/v1/access/', include('permissions.urls')),
+    path('api/v1/jobs/', include('jobs.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('accounts/', include('Accounts.urls')),
-    path('products/', include('Products.urls')),
-    path('system/', include('system_settings.urls')),
-
 ]
+
 
 
