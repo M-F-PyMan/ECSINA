d3bf5b39fe970b777e405063e0fad719bad23865
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-09-17 00:13:56 +0330

    Update urls.py

--- ECSINA/ECSINA_APP/system_settings/urls.py
+++ ECSINA/ECSINA_APP/system_settings/urls.py
@@ -2,9 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import SystemSettingViewSet
 
+app_name='systems'
+
+
 router = DefaultRouter()
 router.register(r'settings', SystemSettingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
