a709b9031cb80e139d1130ad06992d32fe4e9f58
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-09-17 00:24:55 +0330

    Update views.py

--- ECSINA/ECSINA_APP/permissions/views.py
+++ ECSINA/ECSINA_APP/permissions/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .models import Role, Permission, RoleHasPermission
 from .serializers import RoleSerializer, PermissionSerializer, RoleHasPermissionSerializer
@@ -5,11 +6,14 @@ from .serializers import RoleSerializer, PermissionSerializer, RoleHasPermission
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
+    permission_classes = [IsAuthenticated]
 
 class PermissionViewSet(viewsets.ModelViewSet):
     queryset = Permission.objects.all()
     serializer_class = PermissionSerializer
+    permission_classes = [IsAuthenticated]
 
 class RoleHasPermissionViewSet(viewsets.ModelViewSet):
     queryset = RoleHasPermission.objects.all()
     serializer_class = RoleHasPermissionSerializer
+    permission_classes = [IsAuthenticated]
