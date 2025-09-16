d4ec5dd644c12932a088ae339e25772dd068d56f
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-09-17 00:10:29 +0330

    Update urls.py

--- ECSINA/ECSINA_APP/Accounts/urls.py
+++ ECSINA/ECSINA_APP/Accounts/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from .views import UserRegistrationView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', TokenObtainPairView.as_view(), name='user-login'),
 
 
 ]
+
