bd5495ebe8775afad046271cb60d691b339023b1
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-09-17 00:13:15 +0330

    Update urls.py

--- ECSINA/ECSINA_APP/jobs/urls.py
+++ ECSINA/ECSINA_APP/jobs/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import JobViewSet, FailedJobViewSet, JobBatchViewSet
 
+app_name = 'jobs'
+
 router = DefaultRouter()
 router.register(r'jobs', JobViewSet)
 router.register(r'failed-jobs', FailedJobViewSet)
@@ -10,3 +12,4 @@ router.register(r'batches', JobBatchViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
