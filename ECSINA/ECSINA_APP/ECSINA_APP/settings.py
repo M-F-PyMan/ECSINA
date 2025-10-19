03d5fe2810780069f29f490b62eb7f094cc236b9
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-10-19 21:49:11 +0330

    Add suggestion app to installed apps

--- ECSINA/ECSINA_APP/ECSINA_APP/settings.py
+++ ECSINA/ECSINA_APP/ECSINA_APP/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'jobs.apps.JobConfig',
     'rest_framework',
     'rest_framework.authtoken',
+    "suggestion.apps.SuggestionConfig",
 
 ]
 
@@ -122,5 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
