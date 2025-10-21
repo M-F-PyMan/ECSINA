3ab8fd8bbbc6add9beafef7a32ca2346be11eb67
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-10-21 17:35:53 +0330

    Update serializers.py

--- ECSINA/ECSINA_APP/Products/serializers.py
+++ ECSINA/ECSINA_APP/Products/serializers.py
@@ -1,20 +1,17 @@
 from rest_framework import serializers
-from .models import Product, Category,Image
+from .models import Category, Product, Image
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'title', 'parent']
+        fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
-    category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), source='category', write_only=True)
-
     class Meta:
         model = Product
-        fields = ['id', 'title', 'body', 'size', 'format', 'file_path', 'image_path', 'category', 'category_id']
+        fields = '__all__'
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ['id', 'path', 'imageable_type', 'imageable_id', 'uploaded_at']
+        fields = '__all__'
