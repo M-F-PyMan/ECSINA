c6d6d6a772fa769578aab1cedde4919d52aab694
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-10-21 17:33:36 +0330

    Refactor user registration to use viewsets

--- ECSINA/ECSINA_APP/Accounts/views.py
+++ ECSINA/ECSINA_APP/Accounts/views.py
@@ -1,5 +1,15 @@
-from rest_framework import generics
-from .serializers import UserRegistrationSerializer
+from rest_framework import viewsets
+from .models import User, Profile, TempUser
+from .serializers import UserSerializer, ProfileSerializer, TempUserSerializer
 
-class UserRegistrationView(generics.CreateAPIView):
-    serializer_class = UserRegistrationSerializer
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class TempUserViewSet(viewsets.ModelViewSet):
+    queryset = TempUser.objects.all()
+    serializer_class = TempUserSerializer
