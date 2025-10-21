43653cb1b21f2da36bf9eda68904cf9618b94c8f
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-10-21 17:55:43 +0330

    Update urls.py

--- ECSINA/ECSINA_APP/suggestion/urls.py
+++ ECSINA/ECSINA_APP/suggestion/urls.py
@@ -1,8 +1,7 @@
-# suggestions/urls.py
 from rest_framework.routers import DefaultRouter
 from .views import SuggestionTextViewSet
 
 router = DefaultRouter()
-router.register(r'suggestions', SuggestionTextViewSet, basename='suggestion')
+router.register(r'suggestions', SuggestionTextViewSet)
 
 urlpatterns = router.urls
