ecc32002aa174dcae3159fc60dbfe9e846028a93
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-10-21 18:11:45 +0330

    Update urls.py

--- ECSINA/ECSINA_APP/ECSINA_APP/urls.py
+++ ECSINA/ECSINA_APP/ECSINA_APP/urls.py
@@ -1,24 +1,25 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from Accounts import urls
-
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/accounts/', include('accounts.urls')),
-    path('api/v1/products/', include('products.urls')),
-    path('api/v1/system/', include('system_settings.urls')),
-    path('api/v1/access/', include('permissions.urls')),
-    path('api/v1/jobs/', include('jobs.urls')),
+
+    # JWT Authentication
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/v1/', include('suggestions.urls'))
+    #  API Routes
+    path('api/v1/accounts/', include('accounts.urls')),
+    path('api/v1/products/', include('products.urls')),
+    path('api/v1/system/', include("system_settings.urls")),
+    path('api/v1/access/', include('permissions.urls')),
+    path('api/v1/jobs/', include('jobs.urls')),
+    path('api/v1/suggestions/', include('suggestions.urls')),
 ]
 
-
-
-
+#  Static & Media Files (for development)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
