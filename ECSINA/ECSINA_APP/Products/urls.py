e575e16aa399689e68ae25883754e381bd5c3477
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-10-21 17:37:46 +0330

    Update urls.py

--- ECSINA/ECSINA_APP/Products/urls.py
+++ ECSINA/ECSINA_APP/Products/urls.py
@@ -1,18 +1,9 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CategoryViewSet,ImageViewSet
-
-app_name = 'products'
-
+from .views import CategoryViewSet, ProductViewSet, ImageViewSet
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet)
 router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
 router.register(r'images', ImageViewSet)
 
-urlpatterns = [
-    path('', include(router.urls)),
-
-]
-
-
+urlpatterns = router.urls
