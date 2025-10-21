de8ee9ce9e0837121d8a0353714242537b972af6
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-10-21 17:39:14 +0330

    Update views.py

--- ECSINA/ECSINA_APP/jobs/views.py
+++ ECSINA/ECSINA_APP/jobs/views.py
@@ -1,22 +1,15 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
-from .models import Job, FailedJob, JobBatch
-from .serializers import JobSerializer, FailedJobSerializer, JobBatchSerializer
-from permissions.custom import IsAdminRole
-
-class JobViewSet(viewsets.ModelViewSet):
-    queryset = Job.objects.all()
-    serializer_class = JobSerializer
-    permission_classes = [IsAuthenticated]
-
-class FailedJobViewSet(viewsets.ModelViewSet):
-    queryset = FailedJob.objects.all()
-    serializer_class = FailedJobSerializer
-    permission_classes = [IsAuthenticated, IsAdminRole()]
+from .models import JobBatch, Job, FailedJob
+from .serializers import JobBatchSerializer, JobSerializer, FailedJobSerializer
 
 class JobBatchViewSet(viewsets.ModelViewSet):
     queryset = JobBatch.objects.all()
     serializer_class = JobBatchSerializer
-    permission_classes = [IsAuthenticated]
 
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
 
+class FailedJobViewSet(viewsets.ModelViewSet):
+    queryset = FailedJob.objects.all()
+    serializer_class = FailedJobSerializer
