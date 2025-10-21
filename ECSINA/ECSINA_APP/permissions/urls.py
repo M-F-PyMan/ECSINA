03f1860cb9e3aea096a31b72f743d0f2faba0456
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-10-21 17:51:17 +0330

    Update urls.py

--- ECSINA/ECSINA_APP/permissions/urls.py
+++ ECSINA/ECSINA_APP/permissions/urls.py
@@ -1,16 +1,9 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import RoleViewSet, PermissionViewSet, RoleHasPermissionViewSet
-
-app_name = "permission"
-
 
 router = DefaultRouter()
 router.register(r'roles', RoleViewSet)
 router.register(r'permissions', PermissionViewSet)
 router.register(r'role-permissions', RoleHasPermissionViewSet)
 
-urlpatterns = [
-    path('', include(router.urls)),
-]
-
+urlpatterns = router.urls
