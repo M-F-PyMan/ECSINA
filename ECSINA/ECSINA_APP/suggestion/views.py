6fb3973be77b8632b50fcbd649cc0d71aead4059
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-10-21 17:55:17 +0330

    Update views.py

--- ECSINA/ECSINA_APP/suggestion/views.py
+++ ECSINA/ECSINA_APP/suggestion/views.py
@@ -1,5 +1,3 @@
-# suggestions/views.py
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .models import SuggestionText
 from .serializers import SuggestionTextSerializer
@@ -7,4 +5,3 @@ from .serializers import SuggestionTextSerializer
 class SuggestionTextViewSet(viewsets.ModelViewSet):
     queryset = SuggestionText.objects.all()
     serializer_class = SuggestionTextSerializer
-    permission_classes = [IsAuthenticated]
