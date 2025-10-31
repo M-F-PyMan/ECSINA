42017130fe3027839aea470725264b0f3f044bfb
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-11-01 00:27:36 +0330

    Update settings.py

--- ECSINA/ECSINA_APP/ECSINA_APP/settings.py
+++ ECSINA/ECSINA_APP/ECSINA_APP/settings.py
@@ -11,61 +11,57 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from datetime import timedelta
 
-#  Base Directory
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#  Security
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-)ouzyyjwvacnu*!$%pd=1&*xnsx$m1(2=kio3q#4+w)7-q^^0a'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 ALLOWED_HOSTS = []
+AUTH_USER_MODEL = 'Accounts.User'
 
-#  Custom User Model
-AUTH_USER_MODEL = 'accounts.User'
 
-#  Installed Apps
+# Application definition
+
 INSTALLED_APPS = [
-    # Django Core
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # Third-party
-    'rest_framework',
-    'rest_framework.authtoken',
-
-    # Local Apps
-    'accounts.apps.AccountsConfig',
-    'products.apps.ProductsConfig',
+    #our_APP
+    'Accounts.apps.AccountsConfig',
+    'Products.apps.ProductsConfig',
     'system_settings.apps.SystemSettingsConfig',
     'permissions.apps.PermissionsConfig',
     'jobs.apps.JobConfig',
-    'suggestions.apps.SuggestionsConfig',
+    'faq.apps.FaqConfig',
+    'support.apps.SupportConfig',
+    'comments.apps.CommentsConfig',
+    'suggestion.apps.SuggestionConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
+
 ]
 
-# 🔧 REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
 }
 
-#  JWT Settings
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'AUTH_HEADER_TYPES': ('Bearer',),
-}
 
-# Middleware
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -76,19 +72,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# URL & WSGI
 ROOT_URLCONF = 'ECSINA_APP.urls'
-WSGI_APPLICATION = 'ECSINA_APP.wsgi.application'
 
-#  Templates
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -97,43 +90,64 @@ TEMPLATES = [
     },
 ]
 
-#  Database
+WSGI_APPLICATION = 'ECSINA_APP.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # یا 'postgresql'
-        'NAME': 'laravel_db',
-        'USER': 'your_user',
-        'PASSWORD': 'your_password',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
 # Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'Asia/Tehran'
+
 USE_I18N = True
+
 USE_TZ = True
 
-#  Static & Media Files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+HASHID_FIELD_SALT = ')h0p!5&33xmgqas+#w^!xhl4bm#(^qq2$oc5+!6x@0g2y-fyq*'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Hashid Salt (if used)
-HASHID_FIELD_SALT = ')h0p!5&33xmgqas+#w^!xhl4bm#(^qq2$oc5+!6x@0g2y-fyq*'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # برای فایل‌های توسعه
+STATIC_ROOT = BASE_DIR / 'staticfiles'   # برای collectstatic در حالت production
 
-#  Default Primary Key
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-
-
-
-
-
-
