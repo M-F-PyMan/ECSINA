d11887302a930bd6056140edc36245e2ebed2263
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-11-01 00:30:52 +0330

    Implement custom user admin with role selection
    Added custom user admin form and role management.

--- ECSINA/ECSINA_APP/Accounts/admin.py
+++ ECSINA/ECSINA_APP/Accounts/admin.py
@@ -1,26 +1,41 @@
 from django.contrib import admin
-from .models import User,TempUser,Profile
-
+from django import forms
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, TempUser, Profile
+from permissions.models import Role
 
-class UserAdmin(BaseUserAdmin):
+# فرم سفارشی برای تعیین نقش کاربر
+class CustomUserAdminForm(forms.ModelForm):
+    role = forms.ModelChoiceField(
+        queryset=Role.objects.all(),
+        required=False,
+        label="نقش کاربر"
+    )
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+# کلاس ادمین سفارشی با فرم سفارشی
+class CustomUserAdmin(BaseUserAdmin):
+    form = CustomUserAdminForm
     model = User
-    list_display = ('email', 'name', 'mobile', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active')
+    list_display = ('email', 'name', 'mobile', 'role', 'is_staff', 'is_active','deleted_by')
+    list_filter = ('is_staff', 'is_active', 'role')
     fieldsets = (
-        (None, {'fields': ('email', 'name', 'mobile', 'password')}),
+        (None, {'fields': ('email', 'name', 'mobile', 'password', 'role')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'name', 'mobile', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'name', 'mobile', 'password1', 'password2', 'role', 'is_staff', 'is_active')}
         ),
     )
     search_fields = ('email', 'name', 'mobile')
     ordering = ('email',)
 
-admin.site.register(User, UserAdmin)
+# ثبت مدل‌ها در پنل ادمین
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(TempUser)
 admin.site.register(Profile)
-
