fb0b48418d0910ce25a2c6eb679de6a9d25b737a
S.M.kazem.F.M <158856577+M-F-PyMan@users.noreply.github.com>
2025-11-01 00:32:28 +0330

    Update urls.py

--- ECSINA/ECSINA_APP/Accounts/urls.py
+++ ECSINA/ECSINA_APP/Accounts/urls.py
@@ -1,9 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ProfileViewSet, TempUserViewSet
+from django.urls import path
+from .views import UserViewSet, ProfileViewSet, TempUserViewSet, DeleteUserView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'profiles', ProfileViewSet)
 router.register(r'temp-users', TempUserViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('users/<int:user_id>/delete/', DeleteUserView.as_view(), name='delete-user'),
+]
